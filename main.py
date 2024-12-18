256ce1cb25c90fa9ee08e106883ba467c656cee4
ZavyalovIlya <136432841+ZavyalovIlya@users.noreply.github.com>
2024-12-18 20:34:51 +0500

    front via streamlit added
    streamlit_app.py added
    main.py moved into fastapi_app.py
    new main.py
    minor changes in fastapi_app.py

--- main.py
+++ main.py
@@ -1,26 +1,10 @@
-from fastapi import FastAPI, UploadFile, File
-from fastapi.exceptions import HTTPException
-from fastapi.responses import FileResponse
-from code import main
+'''
+to start streamlit use:
+streamlit run streamlit_app.py
 
+to start fastapi use:
+uvicorn fastapi_app:app --reload
 
-app = FastAPI(title="English articles processing")
-
-
-@app.get("/availability check")
-async def app_running():
-	return {"Status": "200 OK"}
-
-
-@app.post("/pdf process")
-async def get_user_pdf(file: UploadFile = File(...)):
-	if file.filename[-3:] != "pdf":
-		raise HTTPException(415, detail="Unsupported Media Type. Please upload pdf file")
-	else:
-		pdf_path = f"Uploaded files/{file.filename}"
-		with open(pdf_path, "wb") as f:
-			content = await file.read()
-			f.write(content)
-		docx_path = main(pdf_path)
-		return FileResponse(path=docx_path, filename=file.filename.split(".")[0]+".docx",
-		                    media_type ="application/vnd.openxmlformats-officedocument.wordprocessingml.document")
+streamlit will be availiable at
+http://localhost:8501/
+'''
